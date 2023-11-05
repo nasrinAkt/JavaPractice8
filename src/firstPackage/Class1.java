1e082251af8fe3226bb81dca5dd5c1df758522c4
Nasrin <test@test.com>
2023-11-05 18:02:21 -0500

    java

---
+++ src/firstPackage/Class1.java
@@ -0,0 +1,23 @@
+package firstPackage;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+	if(99>69) {
+	System.out.println("99 is greater than 69");	
+		
+		
+	}
+	int x = 99;
+	int y = 69;
+	if(x>y) {	
+	System.out.println("x is greater than y");	
+		
+		
+	}
+		
+		
+
+	}
+
+}
---
+++ src/firstPackage/Class2.java
@@ -0,0 +1,25 @@
+package firstPackage;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+	int time = 30;
+	if(time<15) {
+	System.out.println("Good morning");	
+		
+		
+	}else if(time<25) {
+	System.out.println("Good day");	
+		
+		
+	}else {
+	System.out.println("Good evening");	
+		
+		
+	}
+		
+		
+		
+	}
+
+}
